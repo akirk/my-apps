2b6af60f0e87db3852a8a5aa373daad93b46e960
Alex Kirk <akirk@users.noreply.github.com>
2024-05-03 10:47:10 +0200

    -

---
+++ README.md
@@ -0,0 +1,35 @@
+# My Apps
+
+- Contributors: akirk
+- Tags: apps
+- Requires at least: 5.0
+- Tested up to: 6.5
+- License: GPL-2.0-or-later
+- Stable tag: 1.0.0
+
+A WordPress app launcher.
+
+## Description
+
+Plugins that act like local apps can register to be shown in this launcher.
+
+![Screen Recording](img/wordpress-apps.gif)
+
+### Code Example
+
+```php
+
+add_filter( 'my_apps_plugins', function ( $apps ) {
+    // Add your app to the array. These three keys are mandatory:
+    $apps['friends'] = array(
+        // Name: The name that will be displayed.
+        'name'     => __( 'Friends', 'friends' ),
+        // The icon as a URL. You can also use a local URL inside a plugin, using `plugins_url()`.
+        'icon_url' => 'https://ps.w.org/friends/assets/icon-256x256.png',
+        // The URL this should link to.
+        'url'      => home_url( '/friends/' ),
+    );
+    return $apps;
+} );
+```
+
--- my-apps.php
+++ my-apps.php
@@ -10,13 +10,15 @@ class My_Apps {
 		add_action( 'init', array( $this, 'my_apps_endpoint' ) );
 		add_filter( 'query_vars', array( $this, 'my_apps_query_vars' ) );
 		add_filter( 'template_include', array( $this, 'my_apps_template' ) );
-		// ensure that the icon is also shown in mobile view
 		add_action( 'wp_head', array( $this, 'admin_bar_css' ), 50 );
 		add_action( 'admin_head', array( $this, 'admin_bar_css' ), 50 );
 		add_action( 'admin_bar_menu', array( $this, 'admin_bar_menu' ), 1 );
 	}
 
 
+	/**
+	 * Ensure that the admin bar is also shown in the mobile view.
+	 */
 	public function admin_bar_css() {
 		?><style>
 #wpadminbar li#wp-admin-bar-my-apps {
@@ -28,9 +30,14 @@ class My_Apps {
 	}
 }
 </style>
-					<?php
+		<?php
 	}
 
+	/**
+	 * Add the My Apps menu item to the admin bar.
+	 *
+	 * @param mixed $wp_admin_bar The admin bar.
+	 */
 	public function admin_bar_menu( $wp_admin_bar ) {
 		$wp_admin_bar->add_node(
 			array(
@@ -41,15 +48,30 @@ class My_Apps {
 		);
 	}
 
+	/**
+	 * Add the my-apps endpoint.
+	 */
 	public function my_apps_endpoint() {
 		add_rewrite_rule( '^my-apps/?$', 'index.php?my_apps=1', 'top' );
 	}
 
+	/**
+	 * Add the my_apps query var.
+	 *
+	 * @param array $query_vars The query vars.
+	 * @return array The modifiedquery vars.
+	 */
 	public function my_apps_query_vars( $query_vars ) {
 		$query_vars[] = 'my_apps';
 		return $query_vars;
 	}
 
+	/**
+	 * Load the my-apps template.
+	 *
+	 * @param string $template The template.
+	 * @return string The modified template.
+	 */
 	public function my_apps_template( $template ) {
 		global $wp_query;
 		if ( isset( $wp_query->query_vars['my_apps'] ) ) {
@@ -64,9 +86,38 @@ class My_Apps {
 		return $template;
 	}
 
+	/**
+	 * Get the apps to be displayed.
+	 *
+	 * @return array
+	 */
 	public static function get_apps() {
+		/**
+		 * Register your app here.
+		 *
+		 * The key is the plugin slug.
+		 * The value is an array with the following keys:
+		 * - name: The name of the app.
+		 * - icon_url: The URL to the icon.
+		 * - url: The URL to the app.
+		 *
+		 * @param array $apps The apps.
+		 * @return array The modified apps.
+		 *
+		 * Example:
+		 * ```php
+		 * add_filter( 'my_apps_plugins', function( $apps ) {
+		 *   $apps['plugin-slug'] = array(
+		 *       'name' => 'Plugin Name',
+		 *       'icon_url' => 'https://example.com/icon.png',
+		 *       'url' => 'https://example.com/plugin-url',
+		 *   );
+		 * } );
+		 * ```
+		 */
+		$registered_plugins = apply_filters( 'my_apps_plugins', array() );
 		$plugins = array();
-		foreach ( apply_filters( 'my_apps_plugins', array() ) as $plugin => $data ) {
+		foreach ( $registered_plugins as $plugin => $data ) {
 			if ( ! isset( $data['icon_url'], $data['url'], $data['name'] ) ) {
 				continue;
 			}
